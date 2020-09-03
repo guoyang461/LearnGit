16a20fbd5c8bf77e3e4da04810d353cd35ddd8e4
zhaoguoyang <1363799220@qq.com>
2020-09-03 16:34:42 +0800

    Second Commit

---
+++ .idea/LearnGit/Teacher.java
@@ -0,0 +1,28 @@
+public class Teacher {
+
+    String name;
+    String age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+
+
+
+}
